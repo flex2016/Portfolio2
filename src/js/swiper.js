975c77c12269a324ddbad1825bdacd518ce99116
flex2016 <reloded2004@hotmail.com>
2020-09-15 13:01:37 -0400

    added swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,22 @@
+// import Swiper JS
+import Swiper, { Pagination } from "swiper";
+
+// configure Swiper to use modules
+Swiper.use([Pagination]);
+
+const swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
