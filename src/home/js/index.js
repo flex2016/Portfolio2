1c1223c98f92e68fbb4fae57eeb12883f2704317
flex2016 <reloded2004@hotmail.com>
2020-10-08 12:31:42 -0400

    added contact section

--- src/home/js/index.js
+++ src/home/js/index.js
@@ -18,21 +18,50 @@ import "../scss/style.scss";
 Swiper.use([Pagination, Navigation, EffectCoverflow]);
 
 var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
+
+// var swiper = new Swiper(".swiper-container", {
+//   slidesPerView: 4,
+//   centeredSlides: true,
+//   spaceBetween: 30,
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// var swiper = new Swiper(".swiper-container", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
 
 // var swiper = new Swiper(".swiper-container", {
 //   effect: "flip",
